dde9d86c69d16d271ca028fbc00150477d304080
FranzBand <franci.bandieri@gmail.com>
2025-11-21 10:43:26 +0100

    Add files via upload

--- Case_Team2.R
+++ Case_Team2.R
@@ -3,7 +3,7 @@ setwd("~/Desktop/Time Series Econometrics/Case_Team2/Historical-vintages-of-FRED
 
 library(dplyr)
 library(ggplot2)
-
+library(vars)
 
 data_full <- read.csv("2020-01.csv")
 head(data_full)
@@ -34,3 +34,25 @@ trans_cpiaucsl <- diff(diff(log(cpiaucsl))) #transformed according to appendix
 trans_cpiaucsl_1 <- diff(log(cpiaucsl)) #transformed with only one diff
 plot(trans_cpiaucsl_1, main = "transformed with only one diff, cpi")
 plot(trans_cpiaucsl, main = "transformed cpi")
+
+
+#put all data together
+var_data <- cbind(trans_indpro, trans_fedfunds, trans_cpiaucsl)
+View(var_data)
+lag_selection <- VARselect(var_data, lag.max= 15, type ="const")
+print(lag_selection$selection)
+
+##
+var_data_1 <- cbind(trans_indpro, trans_fedfunds)
+View(var_data_1)
+lag_selection_1 <- VARselect(var_data_1, lag.max= 19, type ="const")
+print(lag_selection_1$selection)
+#according to analysis VAR(13) has to be used
+
+##separate data/ var
+
+lag_selection_indpro_10 <- VARselect(trans_indpro, lag.max = 10, type ="const")
+print(lag_selection_indpro_10$selection)
+
+lag_selection_indpro_19 <- VARselect(trans_indpro, lag.max = 19, type ="const")
+print(lag_selection_indpro_19$selection)
