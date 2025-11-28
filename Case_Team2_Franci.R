b1b9c45460e3c84e49a72db23ffe053dacce12e2
Francesco Bandieri <francesco@MacBook-Air-di-Francesco.local>
2025-11-28 23:36:52 +0100

    Implementation of svar with ir responses

--- Case_Team2_Franci.R
+++ Case_Team2_Franci.R
@@ -43,21 +43,6 @@ lag_selection <- VARselect(var_data, lag.max= 60, type ="const")  #check for inf
 print(lag_selection$selection)  #VAR(3) or VAR(4) selected
 
 
-##TO DELETE FROM LINE 46 TO LINE 59
-var_data_1 <- cbind(trans_indpro, trans_fedfunds)
-View(var_data_1)
-lag_selection_1 <- VARselect(var_data_1, lag.max= 19, type ="const")
-print(lag_selection_1$selection)
-#according to analysis VAR(13) has to be used
-
-##separate data/ var
-
-lag_selection_indpro_10 <- VARselect(trans_indpro, lag.max = 10, type ="const")
-print(lag_selection_indpro_10$selection)
-
-lag_selection_indpro_19 <- VARselect(trans_indpro, lag.max = 19, type ="const")
-print(lag_selection_indpro_19$selection)
-
 #use irf to compare models!!!
 #VAR function
 
@@ -68,4 +53,45 @@ var_model
 summary(var_model)
 
 plot(var_model)
+
+##SVAR models by Franci
+
+?SVAR()
+
+# NA = free parameter, 0 = restricted to zero, 1 = fixed (usually diagonal)
+amat <- matrix(NA, 3, 3)
+diag(amat) <- 1        # each variable shocks itself
+amat[2,1] <- 0         # y1 does NOT contemporaneously affect y2
+amat[3,1:2] <- 0       # y1, y2 do NOT contemporaneously affect y3
+amat
+
+svar1 <- SVAR(var_model, estmethod = "direct", Amat = amat)#get svar by a matrix method
+svar1
+summary(svar1)#look at svar1 responses
+
+set.seed(42)
+irf_responsive1 <- irf(svar1, n.ahead = 24)
+irf_responsive1
+plot(irf_responsive1)#plotting the ir function for svar1  
+
+
+var_data_new <- var_data[, c(2,3,1)]  #order data in new way for SVAR
+View(var_data_new)
+
+var_model_new <- VAR(var_data_new, p=3, type = "const")#var model for new order
+
+amat2 <- matrix(NA, 3,3)#a matrix
+diag(amat2) <- 1
+amat2 [2,1] <- 0
+amat2 [3,1:2] <-0
+amat2
+
+svar2 <- SVAR(var_model_new, estmethod = "direct", Amat=amat2)#getting the svar for the new model
+summary(svar2)
+
+irf_responsive2 <- irf(svar2, n.ahead=24)#irf responsive for new order
+plot(irf_responsive2)
+
+#to do:
+#think if order is good/ which order to use and understand how svar works
 
