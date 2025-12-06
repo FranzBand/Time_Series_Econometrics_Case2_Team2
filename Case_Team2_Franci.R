0bf62c2a26654504d0c20d229bc6f5a991ab739f
Francesco Bandieri <francesco@MacBook-Air-di-Francesco.local>
2025-12-07 00:35:44 +0100

    changed order of actions, simplyfied some things.

--- Case_Team2_Franci.R
+++ Case_Team2_Franci.R
@@ -7,6 +7,8 @@ library(ggplot2)
 library(vars)
 library(bootUR)
 
+set.seed(42)
+
 data_full <- read.csv("2020-01.csv")
 head(data_full)
 View(data_full)
@@ -28,13 +30,10 @@ trans_fedfunds <- diff(fedfunds)  #transformed fed rate
 plot(fedfunds, main = "fed fund rates", type= "l", xlab= "Months since Jan 1959", ylab= "index")
 plot(trans_fedfunds, main = " trans fed fund rates", type ="l", xlab= "Months since Jan 1959", ylab= "index")
 
-
-list(fedfunds)
 boxplot(fedfunds, main ="Boxplot fed funds rate")
 boxplot(trans_fedfunds, main ="trans Boxplot fed funds rate")
 
-cpiaucsl <- data$CPIAUCSL  #113
-View(cpiaucsl)
+cpiaucsl <- data$CPIAUCSL  #id113
 plot(cpiaucsl, main = "consumer price index", type= "l", xlab= "Months since Jan 1959", ylab= "index")
 trans_cpiaucsl <- diff(diff(log(cpiaucsl))) #transformed according to appendix
 trans_cpiaucsl_1 <- diff(log(cpiaucsl)) #transformed with only one diff
@@ -43,16 +42,37 @@ plot(trans_cpiaucsl_1, main = "transformed with only one diff, cpi",
 plot(trans_cpiaucsl, main = "transformed cpi",
      xlab= "Months since Jan 1959", ylab= "index", type= "l")
 
+#see length of vectors
+l_indpro <- length(trans_indpro)
+l_indpro 
+
+l_fedfunds <- length(trans_fedfunds)
+l_fedfunds
+
+l_cpiaucsl <- length(trans_cpiaucsl)
+l_cpiaucsl
+
+#get the min length of all vectors
+min_length <- min(l_cpiaucsl, l_fedfunds, l_indpro)
+
+trans_fedfunds <- trans_fedfunds[1:min_length]
+trans_indpro <- trans_indpro[1:min_length]
+
+#check new length
+l_indpro <- length(trans_indpro)
+l_indpro 
+
+l_fedfunds <- length(trans_fedfunds)
+l_fedfunds
+
+l_cpiaucsl <- length(trans_cpiaucsl)
+l_cpiaucsl
 
 #put all data together
 var_data <- cbind(trans_indpro, trans_fedfunds, trans_cpiaucsl)
 View(var_data)
 lag_selection <- VARselect(var_data, lag.max= 60, type ="const")  #check for info criteria
 print(lag_selection$selection)  #VAR(3) or VAR(4) selected
-
-
-#use irf to compare models!!!
-#VAR function
 
 
 #display VAR(3)
@@ -62,29 +82,51 @@ summary(var_model)
 
 plot(var_model)
 
+#new ordering of data
+#var_data_new has the order: fed funds, indpro, cpiauscl
 var_data_new <- var_data[, c(2,1,3)]
+lag_selection_new <- VARselect(var_data_new, lag.max= 60, type ="const")  #check for info criteria
+print(lag_selection$selection)  #VAR(3) or VAR(4) selected
 var_data_new_model <- VAR(var_data_new, p=3, type="const")
-summary(var_data_new_model)
 
+summary(var_data_new_model)
+plot(var_data_new_model)
+
+#impulsive response functions for new order
 irf_var_new <- irf(var_data_new_model, n.ahead = 10, boot= TRUE)
 plot(irf_var_new)
 
 #----Unit root test
-set.seed(42)
 
 ##ur test for indpro
 indpro_log <- log(indpro)
 plot(indpro_log, type="l", main= "ind prod log")
 
-adf_indpro_2 <- adf(diff(indpro_log, differences=2), deterministics= "trend")
-print(adf_indpro_2)
+indpro_d2 <- diff(diff(indpro_log))
+plot(indpro_d2, type="l", main="ind prod diff^2")
+abline(h=0, col= "blue")
 
-adf_indpro_1 <- adf(diff(indpro_log, differences=1), deterministics= "trend")
+indpro_d1 <- diff(indpro_log)
+plot(indpro_d1, type="l", main="ind prod diff^1")
+abline(h=0, col= "blue")
+abline(h=0.01, col="red")
+
+adf_indpro_2 <- adf(diff(indpro_log, differences=2), deterministics= "intercept")
+print(adf_indpro_2)
+#intercept, because no clear trend is visible in the plot.
+#Reject H0 hypothesis, time series is stationary
+
+adf_indpro_1 <- adf(diff(indpro_log, differences=1), deterministics= "intercept")
 print(adf_indpro_1)
+#intercept, because no clear trend is visible in the plot.
+#Reject H0 hypothesis, time series is stationary
 
 adf_indpro_0 <- adf(indpro_log, deterministics= "trend")
 print(adf_indpro_0)
+#trend, because a clear trend is visible in the plot.
+#Fail to reject H0 hypothesis, time series has an unit root
 
+#needed? otherwise delete
 adf_all <- boot_ur(var_data, deterministics= "const")
 print(adf_all)
 
@@ -103,12 +145,13 @@ print(adf_fedfunds_0)
 #cpiaucsl unit root test
 adf_cpiaucsl_2 <- adf(diff(cpiaucsl, differences=2), deterministics= "intercept")
 print(adf_cpiaucsl_2)
+#reject h0 hypothesis, series is stationary of order I(2)
 
 adf_cpiaucsl_1 <- adf(diff(cpiaucsl, differences=1), deterministics= "intercept")
 print(adf_cpiaucsl_1)
-#reject the h0 hypothesis, series is stationary
+#reject the h0 hypothesis, series is stationary of order I(1)
 
-adf_cpiaucsl_0 <- adf(cpiaucsl, deterministics= "intercept")
+adf_cpiaucsl_0 <- adf(cpiaucsl, deterministics= "trend")
 print(adf_cpiaucsl_0)
 #not possible to reject the H0 hypothesis, unit root
 
@@ -123,34 +166,28 @@ amat[2,1] <- 0         # y1 does NOT contemporaneously affect y2
 amat[3,1:2] <- 0       # y1, y2 do NOT contemporaneously affect y3
 amat
 
+#svar 1 has the order: indpro, fed funds, cpiauscl
 svar1 <- SVAR(var_model, estmethod = "direct", Amat = amat)#get svar by a matrix method
 svar1
 summary(svar1)#look at svar1 responses
 
-set.seed(42)
 irf_responsive1 <- irf(svar1, n.ahead = 24)
-irf_responsive1
 plot(irf_responsive1)#plotting the ir function for svar1  
 
-
-var_data_new <- var_data[, c(2,1,3)]  #order data in new way for SVAR
-View(var_data_new)
-
-var_model_new <- VAR(var_data_new, p=3, type = "const")#var model for new order
-
+##svar2
 amat2 <- matrix(NA, 3,3)#a matrix
 diag(amat2) <- 1
 amat2 [2,1] <- 0
 amat2 [3,1:2] <-0
 amat2
 
-svar2 <- SVAR(var_model_new, estmethod = "direct", Amat=amat2)#getting the svar for the new model
+svar2 <- SVAR(var_data_new_model, estmethod = "direct", Amat=amat2)#getting the svar for the new model
 svar2
 summary(svar2)
 
-irf_responsive2 <- irf(svar2, n.ahead=24)#irf responsive for new order
+irf_responsive2 <- irf(svar2, n.ahead=12)#irf responsive for new order, 12 monts ahead
 plot(irf_responsive2)
 
 #to do:
-#think if order is good/ which order to use and understand how svar works
+#understand how svar works
 
